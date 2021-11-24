7449afd01894faf811dafada8376dd3c8874e3e7
AlvinKuruvilla <alvineasokuruvilla@gmail.com>
2021-11-23 23:07:39 -0500

    Minor experiments

--- src/base/log.py
+++ src/base/log.py
@@ -1,4 +1,5 @@
 # Check this link for more details: https://stackoverflow.com/a/23964880
+from colorama.ansi import AnsiFore
 from colorlog import ColoredFormatter
 import logging
 from colorama import Fore, Style
