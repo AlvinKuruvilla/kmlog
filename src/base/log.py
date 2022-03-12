1740e76060d8b8447626753b790360024de158d9
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2022-03-12 11:03:45 -0500

    log.py lint

--- src/base/log.py
+++ src/base/log.py
@@ -1,13 +1,17 @@
+# pylint: disable=R0201
+# pylint: disable=C0301
+# pylint: disable=C0114
+# pylint: disable=W0622
+
 # Copyright 2021 - 2022, Alvin Kuruvilla <alvineasokuruvilla@gmail.com>, Dr. Rajesh Kumar <Rajesh.Kumar@hofstra.edu>
 
 # Use of this source code is governed by an MIT-style
 # license that can be found in the LICENSE file or at
 # https://opensource.org/licenses/MIT.
 
-
-from loguru import logger
 import sys
 from functools import partialmethod
+from loguru import logger
 
 
 class Logger:
