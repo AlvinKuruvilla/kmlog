8d34907c2830d44182f1fedabb0cd82ba2fa4c42
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2022-03-12 11:03:35 -0500

    csv_writer.py pylint

--- src/base/csv_writer.py
+++ src/base/csv_writer.py
@@ -1,3 +1,7 @@
+# pylint: disable=C0301
+# pylint: disable=C0114
+# pylint: disable=C0115
+# pylint: disable=R0201
 # Copyright 2021 - 2022, Alvin Kuruvilla <alvineasokuruvilla@gmail.com>, Dr. Rajesh Kumar <Rajesh.Kumar@hofstra.edu>
 
 # Use of this source code is governed by an MIT-style
@@ -10,16 +14,39 @@ import csv
 class CSVWriter:
     def __init__(self) -> None:
         self.headers = ["Press or Release", "Key", "Time"]
-        return
 
-    def write_header(self, file_path):
-        with open(file_path, "a") as csvfile:
+    def write_header(self, file_path: str):
+        """
+        Write a header to a csv file.
+
+        Parameters
+        ----------
+        text: str
+              The csv file path to write the header to.
+        Returns
+        -------
+        None
+        """
+        with open(file_path, "a", encoding="utf8") as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=self.headers)
             writer.writeheader()
             # print("Wrote header to file: ", file_path)
 
-    def write_data_to_csv(self, file_path, data: list):
-        with open(file_path, "a") as csvfile:
+    def write_data_to_csv(self, file_path: str, data: list):
+        """
+        Write data to a csv file.
+
+        Parameters
+        ----------
+        file_path: str
+              The csv file path to write the data to.
+        data: list
+              The data to write to the file.
+        Returns
+        -------
+        None
+        """
+        with open(file_path, "a", encoding="utf8") as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow(data)
             # print("Wrote data to file: ", file_path)
