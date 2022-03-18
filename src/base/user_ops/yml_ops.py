657adfcca2bcc783bc39740aff9eacb0fa938cee
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2022-03-18 00:21:03 -0400

    Lint yaml_ops file

--- src/base/user_ops/yml_ops.py
+++ src/base/user_ops/yml_ops.py
@@ -1,15 +1,22 @@
+# pylint: disable=C0301
+# pylint: disable=C0114
+# pylint: disable=E0401
+# pylint: disable=C0103
+# pylint: disable=R1710
+
 # Copyright 2021 - 2022, Alvin Kuruvilla <alvineasokuruvilla@gmail.com>, Dr. Rajesh Kumar <Rajesh.Kumar@hofstra.edu>
 
 # Use of this source code is governed by an MIT-style
 # license that can be found in the LICENSE file or at
 # https://opensource.org/licenses/MIT.
 
-from base.backends.yaml_driver import YAMLDriver
 import os
-from base.user_ops.generic_ops import *
+from base.backends.yaml_driver import YAMLDriver
+from base.user_ops.generic_ops import generic_create_user, expand_user_data
 
 
 def user_id_to_yaml_file_path(user_id: str) -> str:
+    """Turn a user_id into a path to a YAML file"""
     # NOTE: We have to deal with the edge case where part of a user_id can exist in another (eg the user_id 123 is in the user_id 1234)
     directory = os.path.join(os.getcwd(), "src", "users")
     for file in os.scandir(directory):
@@ -22,18 +29,19 @@ def user_id_to_yaml_file_path(user_id: str) -> str:
 
 
 def tuple_to_dict(data: tuple, labels: list):
-    assert (
-        len(data) == len(labels),
-        "Cannot create a dictionary from a tuple if the length of the tuple != length of the labels",
-    )
+    """Combine 2 tuples to a single dictionary"""
+    assert len(data) == len(
+        labels
+    ), "Cannot create a dictionary from a tuple if the length of the tuple != length of the labels"
     data_list = list(data)
     data_dict = {}
-    for i in range(len(data_list)):
+    for i in enumerate(data_list):
         data_dict[labels[i]] = data_list[i]
     return data_dict
 
 
 def create_user(user_id: str):
+    """Create a YAML user file from a provided user_id"""
     (
         first,
         last,
@@ -47,7 +55,6 @@ def create_user(user_id: str):
     part = expand_user_data(
         str(gender), str(handedness), str(education_level), str(social_platform)
     )
-    # FIXME: We have to figure out a way so the string fields actually have quotes around them
     pl = list(part)
     pl.insert(0, f'"{str(last)}"')
     pl.insert(0, f'"{str(first)}"')
