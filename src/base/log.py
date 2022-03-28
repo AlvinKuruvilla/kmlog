a1df391430b45582b247d63b5cfdd33634404493
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2022-03-28 17:19:24 -0400

    Add documentation to Logger class

--- src/base/log.py
+++ src/base/log.py
@@ -23,25 +23,85 @@ class Logger:
         logger.add(sys.stderr, format=format)
 
     def km_info(self, msg: str) -> None:
-        """Displays a information level message to the user"""
+        """
+        Displays an information level message to the user
+
+        Parameters
+        ----------
+        msg: str
+            The message to display
+
+        Returns
+        ----------
+        None
+        """
         logger.info(msg)
 
     def km_warn(self, msg: str) -> None:
-        """Displays a warning level message to the user"""
+        """
+        Displays a warning level message to the user
+
+        Parameters
+        ----------
+        msg: str
+            The message to display
+
+        Returns
+        ----------
+        None
+        """
         logger.warning(msg)
 
     def km_error(self, msg: str) -> None:
-        """Displays an error level message to the user"""
+        """
+        Displays an error level message to the user
+
+        Parameters
+        ----------
+        msg: str
+            The message to display
+
+        Returns
+        ----------
+        None
+        """
         logger.error(msg)
 
     def km_fatal(self, msg: str) -> None:
-        """Displays a fatal level message to the user"""
+        """
+        Displays a fatal level message to the user
+
+        Parameters
+        ----------
+        msg: str
+            The message to display
+
+        Returns
+        ----------
+        None
+        """
         logger.level("FATAL", no=33, color="<fg #FFA500>")
         logger.__class__.km_fatal = partialmethod(logger.__class__.log, "FATAL")
         logger.km_fatal(msg)
 
     def km_custom(self, msg: str, markup_tag: str, heading: str = "CUSTOM") -> None:
-        """Display messages simply in red"""
+        """
+        Display a custom colored message
+
+        Parameters
+        ----------
+        msg: str
+            The message to display
+        markup_tag: str
+            The color markup tag to use when displaying to determine the color of the output message
+
+        heading: str
+            The heading to display next to the message. Default is "CUSTOM"
+
+        Returns
+        ----------
+        None
+        """
         logger.level(heading, no=34, color=markup_tag)
         logger.__class__.km_custom = partialmethod(logger.__class__.log, heading)
         logger.km_custom(msg)
