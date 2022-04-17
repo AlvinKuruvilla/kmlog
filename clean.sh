ceb08bfcc0d1b8010b65b8f4094eb62d9ac9cc73
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2022-04-17 00:15:42 -0400

    Silence command output and print helpful information

--- clean.sh
+++ clean.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 echo "Cleaning build directories..."
-rm -r dist/
-rm -r build/
+rm -r dist/ > /dev/null 2>&1
+rm -r build/ > /dev/null 2>&1
+if [ $? -ne 0 ]
+then
+    echo "Nothing to clean..."
+else
+    echo "An error occurred while cleaning"
+fi
