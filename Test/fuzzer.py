d5d76e05c30791c58076be027a522e39c783525a
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2022-04-27 02:14:00 -0400

    The test suite can run the kmlog binary
    This should in theory allow us to send data directly for the executable
    toact on, for example key presses using the keyboard or pynput modules.
    This could allow us to do some interesting pagination-esk testing and
    fuzzing of some kind

--- Test/fuzzer.py
+++ Test/fuzzer.py
@@ -1,89 +1,32 @@
-# A fuzzer based on https://www.fuzzingbook.org/html/Fuzzer.html
+# Copyright 2022, Alvin Kuruvilla <alvineasokuruvilla@gmail.com>, Dr. Rajesh Kumar <Rajesh.Kumar@hofstra.edu>
 
-import random
-from typing import List, Tuple, Any
+# Use of this source code is governed by an MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
+
 import subprocess
+import os
 
 
-class Runner:
-    """Base class for testing inputs."""
-
-    # Test outcomes
-    PASS = "PASS"
-    FAIL = "FAIL"
-    UNRESOLVED = "UNRESOLVED"
-
-    def __init__(self) -> None:
-        """Initialize"""
+class CommandExecutor:
+    def __init__(self, executable_path: str):
+        self.executable_path = executable_path
         pass
 
-    def run(self, inp: str) -> Any:
-        """Run the runner with the given input"""
-        return (inp, Runner.UNRESOLVED)
+    def set_executable_path(self, path):
+        self.executable_path = path
 
+    def get_executable_path(self):
+        return self.executable_path
 
-class PrintRunner(Runner):
-    """Simple runner, printing the input."""
-
-    def run(self, inp) -> Any:
-        """Print the given input"""
-        print(inp)
-        return (inp, Runner.UNRESOLVED)
-
-
-class Fuzzer:
-    """Base class for fuzzers."""
-
-    Outcome = str
-
-    def __init__(self) -> None:
-        """Constructor"""
-        pass
-
-    def fuzz(self) -> str:
-        """Return fuzz input"""
-        return ""
-
-    def run(
-        self, runner: Runner = Runner()
-    ) -> Tuple[subprocess.CompletedProcess, Outcome]:
-        """Run `runner` with fuzz input"""
-        return runner.run(self.fuzz())
-
-    def runs(
-        self, runner: Runner = PrintRunner(), trials: int = 10
-    ) -> List[Tuple[subprocess.CompletedProcess, Outcome]]:
-        """Run `runner` with fuzz input, `trials` times"""
-        return [self.run(runner) for i in range(trials)]
-
-
-class RandomFuzzer(Fuzzer):
-    """Produce random inputs."""
-
-    def __init__(
-        self,
-        min_length: int = 10,
-        max_length: int = 100,
-        char_start: int = 32,
-        char_range: int = 32,
-    ) -> None:
-        """Produce strings of `min_length` to `max_length` characters
-        in the range [`char_start`, `char_start` + `char_range`)"""
-        self.min_length = min_length
-        self.max_length = max_length
-        self.char_start = char_start
-        self.char_range = char_range
-
-    def fuzz(self) -> str:
-        string_length = random.randrange(self.min_length, self.max_length + 1)
-        out = ""
-        for i in range(0, string_length):
-            out += chr(
-                random.randrange(self.char_start, self.char_start + self.char_range)
-            )
-        return out
+    def run_executable(self):
+        popen = subprocess.Popen([self.executable_path])
+        popen.wait()
+        # output = popen.stdout.read()
+        # print(output)
 
 
 if __name__ == "__main__":
-    fuzzer = RandomFuzzer(min_length=10, max_length=20, char_start=65, char_range=26)
-    print(fuzzer.fuzz())
+    path = os.path.join(os.getcwd(), "dist", "kmlog")
+    command_executor = CommandExecutor(path)
+    command_executor.run_executable()
